aa0d61a79e81445f5a1d1aeb514603786417f608
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-11-16 19:36:35 -0800

    got things showing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import fetchWeatherByCity from './services/weatherServices';
 
+interface WeatherData {
+  temperature: number,
+  description: string
+}
+
 function App() {
+
+  let [weather, setWeather] = useState<WeatherData | null>(null);
+
+
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        const weatherResponse = await fetchWeatherByCity("Boston");
+        if (weatherResponse)  {
+          setWeather({
+            temperature: weatherResponse.main.temp,
+            description: weatherResponse.weather[0].description
+          })
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getWeather();
+  }, []) 
+
+
   return (
     <div>
-      {JSON.stringify(fetchWeatherByCity('Boston'))}
+      <p>Temperature:</p>
+      <p>{weather ? weather.temperature : "Loading..."}</p>
+      <p>Description:</p>
+      <p>{weather ? weather.description : "Loading..."}</p>
     </div>
   );
 }
--- src/services/weatherServices.ts
+++ src/services/weatherServices.ts
@@ -1,20 +1,36 @@
-import axios from "axios";
+import axios from "axios";  //import axios to fetch api info (doesn't have to be axios but its the most common)
 //const axios = require('axios'); //old way, only necessary to test
 
 const Base_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
-const fetchWeatherByCity = async (city: string) => {
+//create an interface to specify what the api response looks like(typescript only)
+//my weatherapi response adhere to shape the criteria 
+//specifies what something should have, doesn't care what it might have
+interface WeatherAPIResponse{
+    main: {
+        temp: number;
+    }
+    weather: [{
+        description: string;
+    }];
+}   
+
+
+const fetchWeatherByCity = async (city: string): Promise<WeatherAPIResponse | null> => {  //returns => //promise<this | promsie>that
+    //always good practice to use try/catch when calling an api
+    //error handling 
+    console.log("fetchWeatherByCity had run");
     try {
         const response = await axios.get(`${Base_URL}`, {
             params: {
                 q: city, 
                 units: "imperial",
-                appid: API_KEY
+                appid: API_KEY, 
             }
         });
         return response.data;
-    }   catch (error) {
+    }   catch (error) {         //if there is an error it can give you more ifo on it while still running the code 
         console.error("Error fetching Data: ", error);
         //always have api call related functions return something 
         return null;
