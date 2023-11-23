1080a18175f809fbef44b0060985d8bc5b68d592
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-11-22 18:36:51 -0800

    tried to add things

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import fetchWeatherByCity from './services/weatherServices';
+import { fetchWeatherByCity, fetchForecast } from './services/weatherServices';
 
 interface WeatherData {
   temperature: number,
@@ -8,10 +8,52 @@ interface WeatherData {
   humidity: number
 }
 
+interface ForecastData {
+  list:   {
+      dt_txt: string;
+      main: {
+        temp: number;
+        humidity: number;
+      };
+      weather: [
+        {
+          description: string;
+        }
+      ];
+  } [];
+}
+
 function App() {
 
   let [weather, setWeather] = useState<WeatherData | null>(null);
+  let [forecast, setForecast] = useState<Array<{
+    time: string;
+    temperature: number;
+    description: string;
+    humidity: number;
+  }> | null>(null); 
 
+  useEffect(() => {
+    const getForecast = async () => {
+      try {
+        const forecastResponse = await fetchForecast("Boston");
+        if (forecastResponse) {
+          const upcomingForecast = forecastResponse.list.slice(0, 8).map((item) => ({
+            time: item.dt_txt,
+            temperature: item.main.temp,
+            description: item.weather[0].description,
+            humidity: item.main.humidity,
+          }));
+          
+          setForecast(upcomingForecast);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    getForecast();
+  }, []);
 
   useEffect(() => {
     const getWeather = async () => {
@@ -32,7 +74,6 @@ function App() {
     getWeather();
   }, []) 
 
-
   return (
     <div>
       <p>Temperature:</p>
@@ -40,11 +81,23 @@ function App() {
       <p>Description:</p>
       <p>{weather ? weather.description : "Loading..."}</p>
       <p>Humidity:</p>
-      <p>{weather ? weather.humidity: "Loading..."}</p>
+      <p>{weather ? weather.humidity : "Loading..."}</p>
+      
+      <p>Forecast:</p>
+      {forecast ? (
+        forecast.map((item, index) => (
+          <div key={index}>
+            <p>Time: {item.time}</p>
+            <p>Temperature: {item.temperature}</p>
+            <p>Description: {item.description}</p>
+            <p>Humidity: {item.humidity}</p>
+          </div>
+        ))
+      ) : (
+        "Loading..."
+      )}
     </div>
   );
 }
 
 export default App;
-
-
--- src/services/weatherServices.ts
+++ src/services/weatherServices.ts
@@ -1,8 +1,40 @@
 import axios from "axios";  //import axios to fetch api info (doesn't have to be axios but its the most common)
 //const axios = require('axios'); //old way, only necessary to test
 
-const Base_URL = "https://api.openweathermap.org/data/2.5/weather";
+interface ForecastAPIResponse {
+    list:   {
+        dt_txt: string;
+        main: {
+          temp: number;
+          humidity: number;
+        };
+        weather: [
+          {
+            description: string;
+          }
+        ];
+    } [];
+}
+
+const Base_URL = "https://api.openweathermap.org/data/2.5/forecast";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+
+const fetchForecast = async (city: string): Promise<ForecastAPIResponse | null> => {
+    try {
+        const response = await axios.get(`${Base_URL}`, {
+            params: {
+                q: city,
+                units: "imperial",
+                appid: API_KEY,
+            }
+        }); 
+        return response.data;        
+    }   catch (error) {
+        console.error("Error fetching forecast data: ", error);
+        return null;
+    }
+};
 
 //create an interface to specify what the api response looks like(typescript only)
 //my weatherapi response adhere to shape the criteria 
@@ -23,7 +55,7 @@ const fetchWeatherByCity = async (city: string): Promise<WeatherAPIResponse | nu
     //error handling 
     console.log("fetchWeatherByCity had run");
     try {
-        const response = await axios.get(`${Base_URL}`, {
+        const response = await axios.get(`${Base_URL}weather`, {
             params: {
                 q: city, 
                 units: "imperial",
@@ -39,4 +71,4 @@ const fetchWeatherByCity = async (city: string): Promise<WeatherAPIResponse | nu
 }
 
 //console.log(fetchWeatherByCity('Boston').weather[0].description);
-export default fetchWeatherByCity;
+export { fetchWeatherByCity, fetchForecast }
